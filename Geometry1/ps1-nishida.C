5ca77851fedbce8cdcc3688eb0f5e56c58818b46
gnishida <gnishida@purdue.edu>
2014-01-29 18:24:30 -0500

    Remove unnecessary lines.

--- Geometry1/ps1-nishida.C
+++ Geometry1/ps1-nishida.C
@@ -5,6 +5,9 @@ using namespace std;
 
 /**
  * Return the squared length of the vector.
+ *
+ * @param vector	the vector
+ * @return			the squared length of the vector
  */
 Parameter lengthSquared(const PV2 &vector) {
 	return vector.dot(vector);
@@ -14,6 +17,13 @@ Parameter lengthSquared(const PV2 &vector) {
  * Given two points, compute the minimum disc that contains all the points.
  * The given two points have to be on the boundary of the minimum disc.
  * If a point p is outside the disc, we should create a disc such that p is on its boundary.
+ *
+ * @param points		the point list
+ * @param num			only the first num points are used for this computation.
+ * @param p1			one of the given two points which should be on he boundary of the minimum disc
+ * @param p2			the other point
+ * @param center		the resulting center of the minimum disc
+ * @param radiusSquared	the resulting squared radius of the minimum disc
  */
 void minDiscWith2Points(Points &points, int num, Point *p1, Point *p2, PV2 &center, Parameter &radiusSquared) {
 	center = (p1->getP() + p2->getP()) * 0.5;
@@ -31,6 +41,12 @@ void minDiscWith2Points(Points &points, int num, Point *p1, Point *p2, PV2 &cent
  * Given one point, compute the minimum disc that contains all the points.
  * The given point has to be on the boundary of the minimum disc.
  * If a point p is outside the disc, we should create a disc such that p is on its boundary.
+ *
+ * @param points		the point list
+ * @param num			only the first num points are used for this computation.
+ * @param p				the given point which should be on he boundary of the minimum disc
+ * @param center		the resulting center of the minimum disc
+ * @param radiusSquared	the resulting squared radius of the minimum disc
  */
 void minDiscWithPoint(Points &points, int num, Point *p, PV2 &center, Parameter &radiusSquared) {
 	center = (p->getP() + points[0]->getP()) * 0.5;
@@ -38,12 +54,6 @@ void minDiscWithPoint(Points &points, int num, Point *p, PV2 &center, Parameter 
 		
 	for (int i = 1; i < num; ++i) {
 		if (lengthSquared(points[i]->getP() - center) > radiusSquared) {
-			/*
-			Points points2;
-			points.reserve(i - 1);
-			copy(points.begin(), points.begin() + i, back_inserter(points2));
-			*/
-
 			minDiscWith2Points(points, i, p, points[i], center, radiusSquared);
 		}
 	}
@@ -52,6 +62,10 @@ void minDiscWithPoint(Points &points, int num, Point *p, PV2 &center, Parameter 
 /**
  * Compute the minimum disc that contains all the given points.
  * The center and the radius of the minimum disc will be returned as arguments of this function.
+ *
+ * @param points		the point list that should be enclosed by the minimum disc
+ * @param center		the resulting center of the minimum disc
+ * @param radius		the resulting radius of the minimum disc
  */
 void minDisc(Points &points, PV2 &center, Parameter &radius) {
 	center = (points[0]->getP() + points[1]->getP()) * 0.5;
@@ -59,13 +73,6 @@ void minDisc(Points &points, PV2 &center, Parameter &radius) {
 
 	for (int i = 2; i < points.size(); ++i) {
 		if (lengthSquared(points[i]->getP() - center) > radiusSquared) {
-			/*
-			Points points2;
-			points.reserve(i - 2);
-			copy(points.begin(), points.begin() + i, back_inserter(points2));
-			*/
-
-			//minDiscWithPoint(points2, points[i], center, radiusSquared);
 			minDiscWithPoint(points, i, points[i], center, radiusSquared);
 		}
 	}
